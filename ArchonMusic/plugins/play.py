a9110bb314ba1640686cb98af9b97a87147d1256
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-09-09 21:16:20 +0530

    Add files via upload

--- ArchonMusic/plugins/play.py
+++ ArchonMusic/plugins/play.py
@@ -125,8 +125,8 @@ async def play_hndlr(
             # valid media for this video (rare).
             file.file_path = await yt.stream_url(file.id, video=video)
             if not file.file_path:
-                await sent.edit_text("⏳ Pʀᴇᴘᴀʀɪɴɢ sᴛʀᴇᴀᴍ...")
-                file.file_path, _ = await yt.download(file.id, video=video)
+                await sent.edit_text(m.lang["play_downloading"])
+                file.file_path = await yt.download(file.id, video=video)
 
     await ArchonMusic.play_media(chat_id=m.chat.id, message=sent, media=file)
     if not tracks:
