6fd5ba7c7165d2c62b8a67a41ff2ae9d19c091f0
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-09-13 19:01:38 +0530

    Add files via upload

--- ArchonMusic/helpers/_dataclass.py
+++ ArchonMusic/helpers/_dataclass.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2025-present by TheAloneTeam@Github, < https://github.com/TheAloneTeam >.
+#
+# This file is part of < https://github.com/TheAloneTeam/KartikMusic > project,
+# and is released under the "MIT License".
+# Please see < https://github.com/TheAloneTeam/KartikMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from dataclasses import dataclass
 
 
@@ -11,6 +21,7 @@ class Media:
     title: str = None
     url: str = None
     time: int = 0
+    played_at: float = None
     user: str = None
     video: bool = False
 
@@ -26,6 +37,7 @@ class Track:
     file_path: str = None
     message_id: int = 0
     time: int = 0
+    played_at: float = None
     thumbnail: str = None
     user: str = None
     view_count: str = None
